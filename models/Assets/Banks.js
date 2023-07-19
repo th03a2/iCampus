161be608e0e6ad00a6c60fc796c26005e3007c1d
Phantom2541 <technowiz.thomas.iii.pajarillaga@gmail.com>
2023-07-19 19:52:52 +0800

    update all

---
+++ models/Assets/Banks.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+
+const modelSchema = new mongoose.Schema(
+  {
+    branchId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Branches",
+      required: true,
+    },
+    subjectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Subjects",
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+    },
+    type: {
+      type: String,
+      enum: {
+        values: ["mc", "essay", "enum", "boolean"], // Multiple choice, essay, Enumeration, true/false, false
+        message: "{VALUE} is not supported",
+      },
+      default: "mc",
+    },
+    cluster: { type: String },
+    category: { type: String },
+    message: {
+      type: String,
+      enum: {
+        values: ["easy", "moderate", "hard"], // Multiple choice, essay, Enumeration, true/false, false
+        message: "{VALUE} is not supported",
+      },
+      default: "easy",
+    },
+    question: { type: String },
+    mcAnswers: {
+      a: {
+        type: String,
+      },
+      b: {
+        type: String,
+      },
+      c: {
+        type: String,
+      },
+      d: {
+        type: String,
+      },
+    },
+    boolAnswer: {
+      true: {
+        type: String,
+      },
+      false: {
+        type: String,
+      },
+    },
+    correctAnswer: { type: String },
+    deletedAt: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+modelSchema.query.byCompanyId = function (companyId) {
+  return this.where({ companyId });
+};
+modelSchema.query.byCompanies = function (companies) {
+  return this.where({ companyId: { $in: companies } });
+};
+
+const Entity = mongoose.model("levels", modelSchema);
+
+module.exports = Entity;
